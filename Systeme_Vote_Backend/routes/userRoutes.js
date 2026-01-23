117b34c38078b94fc76b88541575aca052e10984
ABDERRAHIM TONZAR <abdoutonzar@gmail.com>
2026-01-23 21:06:06 +0100

    chore: reset git index

--- Systeme_Vote_Backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { updateUser, getUserById } = require("../controllers/userController");
-const { auth } = require("../middleware/auth");
-
-router.get("/", auth, getUserById);
-router.put("/update", auth, updateUser);
-
-module.exports = router;
--- Systeme_Vote_Backend/routes/voteRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const voteController = require("../controllers/voteController");
-const { auth } = require("../middleware/auth");
-router.post("/insert", auth, voteController.insertVote);
-
-module.exports = router;
